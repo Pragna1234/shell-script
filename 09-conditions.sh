e683281f208c55b3c9fc54720670157d1305eea3
Pragna1234 <alwala.pragna@gmail.com>
2024-04-18 15:40:43 -0700

    hello variable with PERSON1

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -6,3 +6,8 @@ echo "Given number $Number is greater then 10."
 else [$Number -lt 10]
 echo "Given number $Number is less then 10."
 fi
+#-gt ---->greater then
+#-lt---->less then 
+#-eq---->equal to
+#-ge----->greater then equal to 
+#-le----->less then equal to 
---
+++ trying.sh
@@ -0,0 +1,6 @@
+movies=("rrr" "Kushi""Badri")
+
+echo "First movie is ${movies[01]}"
+
+echo "second movie is ${movies[1]}"
+echo "everything is ${movies[@]}"
