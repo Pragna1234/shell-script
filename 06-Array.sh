a5e303a3832d2c3d9455a220d1b0078a49cb56c6
Pragna1234 <alwala.pragna@gmail.com>
2024-04-17 20:56:06 -0700

    hello variable with PERSON1

---
+++ 06-Array.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+MOVIES=("RRR" "MURARI" "KUSHI")
+
+#size of array is 3
+#index are 0,1,2
+#list always starts with 0.
+
+echo "First Movie= ${MOVIES[0]}"
+echo "Second Movie= ${MOVIES[1]}"
+echo "Third Movie= ${MOVIES[@]}"
