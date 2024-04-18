8a0f988bd1d0d2832648c668964c10fded14029b
Pragna1234 <alwala.pragna@gmail.com>
2024-04-17 21:27:31 -0700

    hello variable with PERSON1

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -4,6 +4,7 @@ Number2=$2
 
 #no spaces should be given in between,to get variable active use $ 
 
-SUM=$(($Number1 + $Number2))
+
+SUM=$(($Numner1+$Number2))
 
 echo "Total of $Number1 and $Number2 is $SUM"
