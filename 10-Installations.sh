635d1720e64fdf526e157c82d2a1f55e43d7bba7
Pragna1234 <alwala.pragna@gmail.com>
2024-04-18 16:24:16 -0700

    hello variable with PERSON1

---
+++ 10-Installations.sh
@@ -0,0 +1,31 @@
+# !/bin/bash
+
+USERNAME=$(id -u)
+
+if [$? -ne 0]
+then
+echo "Please run this command with super user"
+exit 1
+else
+echo "proceed with next command."
+fi
+
+
+dnf install mysql-server -y
+
+if [$? -ne 0]
+then
+echo "MYSQL Installation is failed"
+exit 1
+else
+echo "Installation of MYSQL is success."
+fi
+
+
+dnf install git -y
+if [$? -ne 0]
+then
+echo "git installion is failed."
+else
+echo "git installation is success."
+fi
--- trying.sh
+++
@@ -1,6 +0,0 @@
-movies=("rrr" "Kushi" "Badri")
-
-echo "First movie is ${movies[0]}"
-
-echo "second movie is ${movies[1]}"
-echo "everything is ${movies[@]}"
