ce524aed57fd6ba9d00858ea036562fd9b3c27d1
Pragna1234 <alwala.pragna@gmail.com>
2024-04-23 14:48:04 -0700

    hello variable with PERSON1

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,18 +1,22 @@
-# !/bin/bash
+#!/bin/bash
 
+# Define variables
 USERID=$(id -u)
-TIMESTAMP= $(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-VALIDATE(){
-if [ $? -ne 0 ];then
-   echo "$1 is failure"
-   exit 1
-else
-   echo "$2 is success"   
-fi   
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0| cut -d "." -f1)
+LOGFILE="/tmp/$SCRIPT_NAME-$TIMESTAMP.log"
+
+# Function to validate command execution
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 has failed" >> "$LOGFILE"
+        exit 1
+    else
+        echo "$2 was successful" >> "$LOGFILE"
+    fi
 }
 
+# Check if script is executed with root privileges
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
@@ -20,9 +24,10 @@ else
     echo "You are a super user."
 fi
 
+# Install MySQL server
 dnf install mysql-server -y
-VALIDATE $? "MySql is installing"
+VALIDATE $? "MySQL installation"
 
-
+# Install Git
 dnf install git -y
-VALIDATE $? "GIT is Installing"
+VALIDATE $? "Git installation"
