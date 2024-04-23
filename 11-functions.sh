b9c56bba87e54296a9db301193b09799481b6d46
Pragna1234 <alwala.pragna@gmail.com>
2024-04-22 17:17:16 -0700

    hello variable with PERSON1

---
+++ 11-functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VALIDATE(){
+echo "exit status: $1"
+echo "what are you doing: $2"
+}
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+
+VALIDATE $? "Installing mysql"
+fi
+
+dnf install git -y
+
+VALIDATE $? "Installing GIT"
+fi
+
