8bdb193876899ee5e53fe0dd37c29fdc77ba1ee3
Pragna1234 <alwala.pragna@gmail.com>
2024-04-23 14:37:22 -0700

    hello variable with PERSON1

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,9 @@
 # !/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP= $(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
 if [ $? -ne 0 ];then
    echo "$1 is failure"
