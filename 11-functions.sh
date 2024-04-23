a1fcaf98d0dca17bc50e35c6e8b82f444983590e
Pragna1234 <alwala.pragna@gmail.com>
2024-04-22 17:27:38 -0700

    hello variable with PERSON1

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 VALIDATE(){
-echo "exit status: $1"
-echo "what are you doing: $2"
-}
+if [$1 -ne 0]
+then
+    echo "$2 is failure "
+ else
+     echo "$2 is success"
+ fi 
+ }
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
