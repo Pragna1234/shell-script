9ce906c4f8fd3039a5c7489f6632160a7a3629a4
Pragna1234 <alwala.pragna@gmail.com>
2024-04-23 11:37:55 -0700

    hello variable with PERSON1

--- dummypractice.sh
+++ dummypractice.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 User=$(id -u)
-if[$user -ne 0]
+if[ $user -ne 0 ]
 then 
    echo "please change to rootuser"
    exit 1
@@ -9,7 +9,7 @@ else
 fi
 
 dnf install mysql-server -y
-if[$? -ne 0]
+if[ $? -ne 0 ]
 then
    echo "your MYSQL installation is failure"
    exit 1
@@ -18,7 +18,7 @@ else
 fi
 
 dn infstall git -y
-if[$? -ne 0]
+if[ $? -ne 0 ]
 then
    echo "your GIT installation is failure"
    exit 1
