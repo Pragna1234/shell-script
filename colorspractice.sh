efc2a0a8e4bb4cae34b1c89285047147c12d835e
Pragna1234 <alwala.pragna@gmail.com>
2024-04-24 16:13:23 -0700

    hello variable with PERSON1

--- colorspractice.sh
+++ colorspractice.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-USERID=$(id -u)
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
@@ -10,13 +9,6 @@ VALIDATE(){
         echo "$2 is SUCCESS"
     fi
 }
-
-if [ "$USERID" -ne 0 ]; then
-    echo "Please run this script with root user or using sudo."
-    exit 1
-else
-    echo "You are a super user."
-fi
 
 sudo dnf install mysql -y
 VALIDATE $? "Installing MySQL."
