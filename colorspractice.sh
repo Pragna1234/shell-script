ecdaee6f1c630e0143b91641786221d3f1ce8fc6
Pragna1234 <alwala.pragna@gmail.com>
2024-04-24 17:54:36 -0700

    hello variable with PERSON1

--- colorspractice.sh
+++ colorspractice.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SHELL_SCRIPT=$(echo $0 | cut -d "." -f1)
-LOGFILE=/temp/$SHELL_SCRIPT-$TIMESTAMP.
+LOGFILE=/temp/$SHELL_SCRIPT-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
